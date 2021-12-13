2fc7a38dcbd3055df0f0950666e9a58c42e300e6
gaganjot99 <gagankahal00@gmail.com>
2021-12-13 13:20:19 +0530

    error resolving

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ const server = express();
 const PORT = 3000;
 server.listen(PORT, ()=>{console.log("server is running at 3000")});
 
-server.use("/media", server.static("media"));
+server.use("/media", express.static("media"));
 
-router.get("/", (req, res) => {
+server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/src/main.html"));
   });
 
-  router.get("/style.css", (req, res) => {
+  server.get("/style.css", (req, res) => {
     res.sendFile(path.join(__dirname + "/src/style.css"));
   });
-  router.get("/main.js", (req, res) => {
+  server.get("/main.js", (req, res) => {
     res.sendFile(path.join(__dirname + "/src/main.js"));
   });  
